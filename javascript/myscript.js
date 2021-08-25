9ff286b5b4ea698d627ba0a019a1b36f865f5803
gualtierigio <gualtierigio@gmail.com>
2021-08-25 17:01:08 +0200

    completato frizz buzz

--- javascript/myscript.js
+++ javascript/myscript.js
@@ -2,12 +2,12 @@ let listaNumeri = []
 
 for (let i =0; i < 100; i++){
     let n = (i + 1);
-    
+
     if (n % 5 == 0 && n % 3 == 0) {
         listaNumeri.push("fizzbuzz")
     } else if (n % 3 == 0){
         listaNumeri.push("fizz")
-    } else if (n % 5 == 0){
+    } else if ((n) % 5 == 0){
         listaNumeri.push("buzz")
     } else  {
         listaNumeri.push(n);
